6e8392334e552b6dbcbeb5ff1aee013bc5ddd49d
nftug <whitepony1214@gmail.com>
2021-06-14 11:00:49 +0900

    winclose.shにfirefoxを追加

--- home/bin/winclose.sh
+++ home/bin/winclose.sh
@@ -17,7 +17,7 @@ elif [[ ${wintitle} =~ tmux$ ]]; then
     
 else
     case $progname in
-	"chromium"|"nautilus"|"gedit"|"thunar" )
+	"chromium"|"nautilus"|"gedit"|"thunar"|"firefox" )
 	    xdotool key ctrl+w ;;
 	gimp-* )
             winname=`xdotool getwindowname $activewin`
