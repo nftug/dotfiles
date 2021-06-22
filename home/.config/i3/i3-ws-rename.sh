1aeb926fdd3c60586caef9c14d7b221bc271fd3d
nftug <whitepony1214@gmail.com>
2021-06-22 21:50:49 +0900

    i3-wsのアイコンにSlackを追加

--- home/.config/i3/i3-ws-rename.sh
+++ home/.config/i3/i3-ws-rename.sh
@@ -9,7 +9,7 @@ declare -A icon=(
     ['Leafpad']='' ['Viewnior']='' ['Xarchiver']='' ['mpv']='蘆'
     ['Evince']='' ['zoom']='' ['Easytag']='炙' ['Steam']='戮'
     ['Enpass']='' ['Pamac-manager']='' ['Mpdevil']=''
-    ['firefox']=''
+    ['firefox']='' ['Slack']='聆'
     
     ['_EMPTY_']='ﱤ'
     ['_OTHER_']='ﬓ'
