82113cd9d89f0be37e84b8dbe97fe25ee105b3a4
nftug <whitepony1214@gmail.com>
2021-10-16 23:20:12 +0900

    fix some

--- home/.config/i3/i3-ws-rename.sh
+++ home/.config/i3/i3-ws-rename.sh
@@ -9,7 +9,7 @@ declare -A icon=(
     ['Leafpad']='' ['Viewnior']='' ['Xarchiver']='' ['mpv']='蘆'
     ['Evince']='' ['zoom']='' ['Easytag']='炙' ['Steam']='戮'
     ['Enpass']='' ['Pamac-manager']='' ['Mpdevil']=''
-    ['firefox']='' ['Slack']='聆' ['code']=''
+    ['firefox']='' ['Slack']='聆' ['Code']=''
     
     ['_EMPTY_']='ﱤ'
     ['_OTHER_']='ﬓ'
